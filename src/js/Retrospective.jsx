51b5e826a403722ffa67de025dd7d46ab450dadf
Satake <satake@objectfoo.com>
2015-04-16 11:06:00 -0700

    Start List component

--- src/js/Retrospective.jsx
+++ src/js/Retrospective.jsx
@@ -2,17 +2,24 @@
 
 var React = require('react');
 var Header = require('./Header.jsx');
+var List = require('./List.jsx');
 
 var Retrospective = React.createClass({
 	render: function() {
 		return(
-			<Header />
-			// list
-			// list with voting
-			// sort button, print button
-			// list
+			<div className="retrospective">
+				<Header />
+				<List id="good" />
+				<List id="bad" />
+				<List id="next" />
+			</div>
 		);
 	}
 });
+
+// list
+// list with voting
+// sort button, print button
+// list
 
 module.exports = Retrospective;
---
+++ src/js/TextEntry.jsx
@@ -0,0 +1,11 @@
+'use strict';
+
+var React = require('react');
+
+var TextEntry = React.createClass({
+	render: function () {
+		return <div>entry</div>;
+	}
+});
+
+module.exports = TextEntry;
